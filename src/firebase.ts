3ecbdb2db1644ef4e0faa1e4cc715ea0afae4fec
50Start <74140514+50start@users.noreply.github.com>
2021-09-09 21:26:32 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmnFREla-J6mcLOYzr9yxJvuK_a3drsUk",
-  authDomain: "twitter-app-96b73.firebaseapp.com",
-  projectId: "twitter-app-96b73",
-  storageBucket: "twitter-app-96b73.appspot.com",
-  messagingSenderId: "808595097289",
-  appId: "1:808595097289:web:2a7d75414c009740ef2f18",
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
